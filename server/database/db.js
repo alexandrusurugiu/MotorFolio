4efdd37c780fc5660ae1c2bda04e957928c1c26e
Alexandru Surugiu <surugiugeorge22@stud.ase.ro>
2025-12-05 13:08:35 +0200

    firebase initial commit

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+function initializeDatabase() {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    return admin.firestore();
+}
+
+const db = initializeDatabase();
+
+module.exports = db;
