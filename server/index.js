febc2c39db4a70928e76bf4997ca4a0838d35c04
Alex <georgealexandrusurugiu@gmail.com>
2026-01-24 21:26:25 +0200

    -

--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(morgan('dev'));
-app.use(cors{
+app.use(cors({
     origin: ['https://motorfolio.vercel.app', 'http://localhost:5173'], 
     credentials: true
-});
+}));
 app.use(express.json());
 
 app.use('/server/auth', authRoutes);
